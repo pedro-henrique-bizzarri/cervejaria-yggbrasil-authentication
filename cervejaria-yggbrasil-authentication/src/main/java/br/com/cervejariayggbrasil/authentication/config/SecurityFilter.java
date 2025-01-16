e57669a9c9c0a959896b39e11bc222e1679ff85c
Pedro Henrique Bizzarri <pedrobizzarri@gmail.com>
2025-01-16 00:25:21 -0300

    Passando recovery token para jwtservice

--- cervejaria-yggbrasil-authentication/src/main/java/br/com/cervejariayggbrasil/authentication/config/SecurityFilter.java
+++ cervejaria-yggbrasil-authentication/src/main/java/br/com/cervejariayggbrasil/authentication/config/SecurityFilter.java
@@ -29,7 +29,7 @@ public class SecurityFilter extends OncePerRequestFilter{
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-                var token = this.recoverToken(request);
+                var token = jwtService.recoverToken(request);
                 if(token != null){
                     var login = jwtService.validateToken(token);
                     Optional<UserDetails> user = usuarioRepository.findByLogin(login);
@@ -38,12 +38,6 @@ public class SecurityFilter extends OncePerRequestFilter{
                     SecurityContextHolder.getContext().setAuthentication(authentication);
                 }
                 filterChain.doFilter(request, response);
-    }
-
-    private String recoverToken(HttpServletRequest request){
-        var authHeader = request.getHeader("Authorization");
-        if(authHeader == null) return null;
-        return authHeader.replace("Bearer ", "");
     }
 
 }
